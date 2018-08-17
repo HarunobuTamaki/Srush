efd2ad850cb10e91b7e829d9aa7b610e978e785f
Harunobu Tamaki <agunus-dei@hotmail.co.jp>
2018-08-17 22:40:22 +0900

    Srush Playerアニメーション設定

---
+++ Assets/Script/MoveGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround : MonoBehaviour {
+
+    public float speed=-0.1f; //地面を動かすスピード
+    float size = 12f; //地面のサイズ
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
+        if (this.transform.position.x + size < 0)
+        {
+            Debug.Log("out of display");
+            this.transform.Translate(size * 2, 0, 0);
+        }
+	}
+}
---
+++ Assets/Script/PlayerMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMotion : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetBool("Jump", Input.GetButtonDown("Jump"));
+	}
+}
